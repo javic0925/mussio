0db39701719b3900083712fa7eff82c860e75374
Javier Cruz <javic0925@gmail.com>
2022-01-26 21:12:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -30,13 +30,84 @@ const streaming = async () => {
   streaming();
 
   
-  var myAudio = document.getElementById("my-audio");
-  const title = document.querySelector('.titleTrack');
+  var song = document.getElementsByTagName('audio')[0],
+  sourceMp3 = document.getElementsByTagName('audio')[0];
 
-
-      function togglePlay(music){
-      myAudio.setAttribute('src' , music)
+  function togglePlay(music){
+  song.setAttribute('src' , music)
 };
+
+
+// $(function togglePlay (music) {
+//   var song = document.getElementsByTagName('audio')[0],
+//       sourceMp3 = document.getElementsByTagName('audio')[0];
+      
+
+//       sourceMp3.src = music;
+
+
+//   $('#player').click(function (e) {
+     
+//       if (song.paused) song.play();
+//       else 
+//       song.setAttribute('src' , music); 
+  
+//   });
+//   $('#player').bind('click', function() {
+//       if ($('#playback').attr('class') == 'fa fa-stop fa-lg beat')
+//           $('#playback').attr('class', 'fa fa-play fa-lg');
+//       else
+//           $('#playback').attr('class', 'fa fa-stop fa-lg beat');
+//   });
+
+//   song.addEventListener('pause', function () {
+//       song.setAttribute('src' , music);
+//       song.currentTime = 0;
+//       $('#playback').attr('class', 'fa fa-play fa-lg');
+//   });
+
+
+
+  
+// });
+var interval = 400;
+var timer = window.setInterval(function() {
+  
+      var song = document.getElementsByTagName('audio')[0]
+      if (song.currentTime == 0){
+          $('#playback').attr('class', 'fa fa-play fa-lg');
+          
+      }else{
+          $('#playback').attr('class', 'fa fa-stop fa-lg beat');
+          
+          
+      }
+      if (yourCodeHasBeenExecuted === true) {
+        window.clearInterval(timer);
+     }
+      },interval);
+
+
+let mediaSession = navigator.mediaSession;
+
+if ("mediaSession" in navigator){
+  navigator.mediaSession.metadata = new MediaMetadata({
+    title: "Mussio",
+    artist: "JaviC Dev",
+    artwork: [{src: "#"}]
+  });}
+
+
+
+
+
+
+
+
+
+
+
+  
 
   
   
